3640351a4955639d73c9919e01c691c5533482dd
Konrad K Sobon <ksobon1986@gmail.com>
2015-01-01 19:37:47 -0500

    Create Get Line Style By Name.py

---
+++ Dynamo_0.7.X/Get Line Style By Name.py
@@ -0,0 +1,35 @@
+#Copyright(c) 2015, Konrad Sobon
+# @arch_laboratory, http://archi-lab.net
+
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+# Import DocumentManager and TransactionManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+# Import RevitAPI
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+
+doc = DocumentManager.Instance.CurrentDBDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+
+#The inputs to this node will be stored as a list in the IN variable.
+dataEnteringNode = IN
+
+name = IN[0]
+
+lineStyles = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Lines)
+replacementStyles = []
+try:
+	replacementStyles.append(next(i.LineStyle for i in lineStyles if name==i.LineStyle.Name))
+except StopIteration:
+	pass
+
+OUT = replacementStyles
