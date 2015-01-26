6a2ce3db84dafd878b15070c8e70284c28e8ee08
Konrad K Sobon <ksobon1986@gmail.com>
2015-01-25 23:12:15 -0500

    Create getUnplacedGroups.py

---
+++ Dynamo_0.7.X/getUnplacedGroups.py
@@ -0,0 +1,32 @@
+#Copyright(c) 2015, Konrad Sobon
+# @arch_laboratory, http://archi-lab.net
+
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+# Import DocumentManager and TransactionManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+# Import RevitAPI
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+
+doc = DocumentManager.Instance.CurrentDBDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+
+#The inputs to this node will be stored as a list in the IN variable.
+dataEnteringNode = IN
+
+groups = FilteredElementCollector(doc).OfClass(GroupType)
+unplacedGroups = []
+for i in groups:
+	gInstances = i.Groups
+	if gInstances.IsEmpty == 1:
+		unplacedGroups.append(i)
+OUT = unplacedGroups
