e3b63018fa3d9e5cb164eafbe770d1b374247eb6
Konrad K Sobon <ksobon1986@gmail.com>
2015-02-08 11:51:26 -0500

    Create getFilledRegionBoundary.py

---
+++ getFilledRegionBoundary.py
@@ -0,0 +1,43 @@
+#Copyright(c) 2015, Konrad K Sobon
+# @arch_laboratory, http://archi-lab.net
+
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+# Import DocumentManager and TransactionManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+from System.Collections.Generic import *
+
+# Import RevitAPI
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB.Analysis import *
+
+doc = DocumentManager.Instance.CurrentDBDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+
+# Import ToDSType(bool) extension method
+clr.AddReference("RevitNodes")
+import Revit
+clr.ImportExtensions(Revit.Elements)
+
+#The inputs to this node will be stored as a list in the IN variable.
+dataEnteringNode = IN
+
+boundaries, temp_lst = [], []
+collector = FilteredElementCollector(doc, doc.ActiveView.Id).OfClass(FilledRegion)
+for i in collector:
+	if len(i.GetBoundaries()) !=1:
+		boundaries.append([val for subl in i.GetBoundaries() for val in subl])
+	else:
+		boundaries.extend(i.GetBoundaries())
+	
+#Assign your output to the OUT variable
+OUT = boundaries
