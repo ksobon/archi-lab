3478e8e806072bdb6af8ee332a4137b069d6c623
Konrad K Sobon <ksobon1986@gmail.com>
2015-02-10 18:29:54 -0500

    Create Read GH File.py

---
+++ Read GH File.py
@@ -0,0 +1,137 @@
+#Copyright(c) 2015, Konrad Sobon
+# @arch_laboratory, http://archi-lab.net
+
+import clr
+import sys
+clr.AddReference('ProtoGeometry')
+
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+import os
+appDataPath = os.getenv('APPDATA')
+msPath = appDataPath + r"\Dynamo\0.7\packages\Mantis Shrimp\extra"
+"""
+rhPath = appDataPath + r"\Dynamo\0.7\packages\Mantis Shrimp\bin"
+"""
+if msPath not in sys.path:
+	sys.path.Add(msPath)
+"""
+if rhPath not in sys.path:
+	sys.path.Add(rhPath)
+"""
+
+possibleRhPaths = []
+possibleRhPaths.append(r"C:\Program Files\Rhinoceros 5 (64-bit)\System\RhinoCommon.dll")
+possibleRhPaths.append(r"C:\Program Files\Rhinoceros 5.0 (64-bit)\System\RhinoCommon.dll")
+possibleRhPaths.append(r"C:\Program Files\McNeel\Rhinoceros 5.0\System\RhinoCommon.dll")
+possibleRhPaths.append(msPath)
+checkPaths = map(lambda x: os.path.exists(x), possibleRhPaths)
+for i, j in zip(possibleRhPaths, checkPaths):
+	if i not in sys.path and j == True:
+		sys.path.Add(i)
+		clr.AddReferenceToFileAndPath(i)
+
+from Autodesk.DesignScript.Geometry import *
+from System import Array
+from System.Collections.Generic import *
+import Rhino as rc
+import pickle
+from mantisshrimp import *
+
+#The inputs to this node will be stored as a list in the IN variable.
+dataEnteringNode = IN
+filePath = str(IN[0])
+_import = IN[1]
+
+class SerializeObjects(object):
+	def __init__(self, filePath, data = None):
+		self.filePath = filePath
+		self.data = data
+
+	def saveToFile(self):
+		with open(self.filePath, 'wb') as outf:
+			pickle.dump(self.data, outf)
+
+	def readFromFile(self):
+		with open(self.filePath, 'rb') as inf:
+			self.data = pickle.load(inf)
+
+serializer = SerializeObjects(filePath)
+serializer.readFromFile()
+
+# recursive function to process any input list and output 
+# matching structure list
+def ProcessList(_func, _list, _units):
+    return map( lambda x: ProcessList(_func, x, _units) if type(x)==list else _func(x, _units), _list )
+    
+# function to convert MS Objects to DS objects
+def toDSObject(item, units):
+	if type(item) == MSPoint:
+		return item.toDSPoint(units)
+	elif type(item) == MSLine:
+		return item.toDSLine(units)
+	elif type(item) == MSPolyLine:
+		return item.toDSPolyCurve(units)
+	elif type(item) == MSEllipse:
+		return item.toDSEllipse(units)
+	elif type(item) == MSCircle:
+		return item.toDSCircle(units)
+	elif type(item) == MSArc:
+		return item.toDSArc(units)
+	elif type(item) == MSNurbsCurve:
+		return item.toDSNurbsCurve(units)
+	elif type(item) == MSPolyCurve:
+		return item.toDSPolyCurve(units)
+	elif type(item) == MSMesh:
+		return item.toDSMesh(units)
+	elif type(item) == MSNurbsSurface:
+		return item.toDSNurbsSurface(units)
+	#elif type(item) == MSPolySurface:
+		#return item.toDSPolySurface(units)
+	elif type(item) == MSBrep:
+		return item.toDSPolySurface(units)
+	elif type(item) == MSData:
+		return item.data
+	else:
+		return "Geometry type not yet supported"
+
+if _import:
+	serializedData = serializer.data
+	if type(serializedData) == MSData:
+		geometryOut = serializedData.data
+	else:
+		rhUnits = serializedData.pop(0).data
+		geometryOut = ProcessList(toDSObject, serializedData, rhUnits)	
+else:
+	message = "Import set to false"
+OUT = geometryOut
+"""
+test = []
+dsFaces = []
+data = serializer.data[0]
+
+faces = data.faces
+trims = data.trims
+
+for face, loop in zip(faces, trims):
+	trimLoops = []
+	dsFace = face.toDSNurbsSurface(rhUnits)
+	for trim in loop:
+		dsTrims = []
+		for curve in trim:
+			if type(curve) == MSNurbsCurve:
+				#dsTrims.append(curve.toDSNurbsCurve(rhUnits))
+				test.append(curve.toDSNurbsCurve(rhUnits))
+			elif type(curve) == MSMultiSpanNurbsCurve:
+				for i in curve.curves:
+					test.append(i.toDSNurbsCurve(rhUnits))
+			elif type(curve) == MSEllipse:
+				test.append(curve.toDSEllipse(rhUnits))
+		curveArray = List[Curve](test)
+		trimLoops.append(PolyCurve.ByJoinedCurves(curveArray))
+	trimFace = dsFace.TrimWithEdgeLoops(trimLoops)
+
+#Assign your output to the OUT variable
+OUT = trimFace
+"""
