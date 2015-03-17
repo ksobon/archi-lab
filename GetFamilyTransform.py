75e88e387ad0adc2f2fda0e268703e0b09dccfa3
Konrad K Sobon <ksobon1986@gmail.com>
2015-03-17 18:54:59 -0400

    Create GetFamilyTransform.py

---
+++ GetFamilyTransform.py
@@ -0,0 +1,15 @@
+import clr
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
+
+clr.AddReference("RevitNodes")
+import Revit
+clr.ImportExtensions(Revit.GeometryConversion)
+
+items = UnwrapElement(IN[0])
+
+location = []
+for i in items:
+	location.append(i.GetTransform().Origin.ToPoint())
+
+OUT = location
