0ac1defb2ace09aa1e9f0e5fd8ce221d7a9f9147
Konrad K Sobon <ksobon1986@gmail.com>
2015-03-28 19:24:46 -0400

    Update writeExcel.py

--- excel/writeExcel.py
+++ excel/writeExcel.py
@@ -31,8 +31,10 @@ liveStreaming = IN[5]
 cellFill = IN[6]
 borderStyle = IN[7]
 
+# Fill Settings definitions
 def GetLineStyle(key):
-	keys = ["Continuous", "Dash", "DashDot", "DashDotDot", "RoundDot", "SquareDotMSO", "LongDash", "DoubleXL", "NoneXL"]
+	keys = ["Continuous", "Dash", "DashDot", "DashDotDot", "RoundDot", "SquareDotMSO", "LongDash", 
+			"DoubleXL", "NoneXL"]
 	values = [1, -4115, 4, 5, -4118, -4118, -4115, -4119, -4142]
 	d = dict()
 	for i in range(len(keys)):
@@ -42,6 +44,53 @@ def GetLineStyle(key):
 	else:
 		return None
 
+def GetPatternType(key):
+	keys = ["Solid", "75Percent", "50Percent", "25Percent", "10Percent", "Cross", "DarkDownwardDiagonal", 
+		"DarkHorizontal", "DarkUpwardDiagonal", "DarkVertical", "DashedDownwardDiagonal", "DashedHorizontal", 
+		"DashedUpwardDiagonal", "DashedVertical", "DiagonalBrick", "DiagonalCross", "Divot", "DottedDiamond", 
+		"DottedGrid", "DownwardDiagonal", "Horizontal", "HorizontalBrick", "LargeCheckerBoard", "LargeConfetti", 
+		"LargeGrid", "LightDownwardDiagonal", "LightHorizontal", "LightUpwardDiagonal", "LightVertical",
+		"NarrowHorizontal", "NarrowVertical", "OutlinedDiamond", "Plaid", "Shingle", "SmallCheckerBoard", 
+		"SmallConfetti", "SmallGrid", "SolidDiamond", "Sphere", "Trellis", "UpwardDiagonal", "Vertical", 
+		"Wave", "Weave", "WideDownwardDiagonal", "WideUpwardDiagonal", "ZigZag"]
+	values = [-4105, -4125, -4124, -4124, 18, 15, -4121, -4128, -4162, -4166, 10, 10, 10, 10, 16, 16, 10, 16, 
+			15, -4121, -4128, 10, 9, 10, 15, 13, 11, 14, 12, -4128, -4166, 16, 9, 10, 9, 10, 15, 16, 9, 10, -4162, 
+			-4166, 10, 10, -4121, -4162, 10]
+	d = dict()
+	for i in range(len(keys)):
+		d[keys[i]] = values[i]
+	if key in d:
+		return d[key]
+	else:
+		return None
+
+def GetBevel(key):
+	keys = ["None", "Angle", "ArtDeco", "Circle", "Convex", "CoolSlant", "Cross", "Divot", "HardEdge", "RelaxedInset", 
+		"Riblet", "Slope", "SoftRound"]
+	values = [1, 6, 13, 3, 8, 9, 5, 10, 12, 2, 11, 4, 7]
+	d = dict()
+	for i in range(len(keys)):
+		d[keys[i]] = values[i]
+	if key in d:
+		return d[key]
+	else:
+		return None
+
+def RGBToHex(rgb):
+	strValue = '%02x%02x%02x' % rgb
+	iValue = int(strValue, 16)
+	return iValue
+"""	
+paramList = cellFill.split("~")
+
+patternType = GetPatternType(paramList[0])
+bColors = paramList[1].split(",")
+backColor = RGBToHex((int(bColors[0]), int(bColors[1]), int(bColors[2])))
+fColors = paramList[2].split(",")
+foreColor = RGBToHex((int(fColors[0]), int(fColors[1]), int(fColors[2])))
+opacity = int(paramList[3])
+bevelType = GetBevel(paramList[4])
+"""
 def LiveStream():
 	try:
 		xlApp = Marshal.GetActiveObject("Excel.Application")
@@ -51,11 +100,6 @@ def LiveStream():
 		return xlApp
 	except:
 		return None
-
-def RGBToHex(rgb):
-	strValue = '%02x%02x%02x' % rgb
-	iValue = int(strValue, 16)
-	return iValue
 
 def WriteData(ws, data, byColumn, cellFill, borderStyle):
 	originX = ws.UsedRange.Row
