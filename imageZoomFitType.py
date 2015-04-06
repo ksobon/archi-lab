4a6c823f465a67cbda489225f4d06dc7fc726896
Konrad K Sobon <ksobon1986@gmail.com>
2015-04-06 14:44:36 -0400

    Create imageZoomFitType.py

---
+++ imageZoomFitType.py
@@ -0,0 +1,21 @@
+#Copyright(c) 2015, Konrad Sobon
+# @arch_laboratory, http://archi-lab.net
+
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+# Import DocumentManager and TransactionManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+# Import RevitAPI
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+import System
+
+#Assign your output to the OUT variable
+OUT = System.Enum.GetValues(ZoomFitType)
