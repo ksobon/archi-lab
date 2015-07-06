e01ae309a9d05784d670079f2d74684359187186
Konrad K Sobon <ksobon1986@gmail.com>
2015-07-06 11:16:55 -0400

    Create viewSets.py

---
+++ 005_PrintingPDFs/viewSets.py
@@ -0,0 +1,25 @@
+#Copyright(c) 2015, Konrad Sobon
+# @arch_laboratory, http://archi-lab.net
+
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+# Import DocumentManager and TransactionManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+doc = DocumentManager.Instance.CurrentDBDocument
+
+# Import RevitAPI
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+import System
+
+ps = FilteredElementCollector(doc).OfClass(PrintSetting)
+
+#Assign your output to the OUT variable
+OUT = [i.Name for i in ps]
