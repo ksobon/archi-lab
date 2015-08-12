f9f3c3d1dd638977987627ef77fbc948d083f229
Konrad K Sobon <ksobon1986@gmail.com>
2015-08-12 01:00:29 -0400

    Create setFillPatternName.py

---
+++ setFillPatternName.py
@@ -0,0 +1,44 @@
+#Copyright(c) 2015, Konrad Sobon
+# @arch_laboratory, http://archi-lab.net
+
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+# Import ToDSType(bool) extension method
+clr.AddReference("RevitNodes")
+import Revit
+clr.ImportExtensions(Revit.Elements)
+
+# Import DocumentManager and TransactionManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+from System.Collections.Generic import *
+
+# Import RevitAPI
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+
+doc = DocumentManager.Instance.CurrentDBDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+
+#The inputs to this node will be stored as a list in the IN variable.
+dataEnteringNode = IN
+
+fillPatElems = []
+for i in IN[0]:
+	fillPatElems.append(UnwrapElement(i))
+values = IN[1]
+
+TransactionManager.Instance.EnsureInTransaction(doc)
+for i, j in zip(fillPatElems, values):
+	i.Name = j
+TransactionManager.Instance.TransactionTaskDone()
+
+#Assign your output to the OUT variable
+OUT = fillPatElems
