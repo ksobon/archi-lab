0bb8c4ed60f96e6d1cf48c0e272b08064358ef14
Konrad K Sobon <ksobon1986@gmail.com>
2015-08-21 10:56:45 -0400

    Update currentDocument.py

--- Dynamo_0.8.X/currentDocument.py
+++ Dynamo_0.8.X/currentDocument.py
@@ -6,10 +6,24 @@ import clr
 clr.AddReference("RevitServices")
 import RevitServices
 from RevitServices.Persistence import DocumentManager
-doc = DocumentManager.Instance.CurrentDBDocument
+
+import sys
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
 
+try:
+	errorReport = None
+	doc = DocumentManager.Instance.CurrentDBDocument
+except:
+	# if error accurs anywhere in the process catch it
+	import traceback
+	errorReport = traceback.format_exc()
+
 #Assign your output to the OUT variable
-OUT = doc
+if errorReport == None:
+	OUT = doc
+else:
+	OUT = errorReport
