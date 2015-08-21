ac63cb9319952e47a3f93f14551f9e71173375b2
Konrad K Sobon <ksobon1986@gmail.com>
2015-08-21 16:02:35 -0400

    Update getElementsByCategoryLevel.py

--- Dynamo_0.8.X/getElementsByCategoryLevel.py
+++ Dynamo_0.8.X/getElementsByCategoryLevel.py
@@ -1,27 +1,17 @@
 #Copyright(c) 2015, Konrad K Sobon
 # @arch_laboratory, http://archi-lab.net
 
-import clr
-clr.AddReference('ProtoGeometry')
-from Autodesk.DesignScript.Geometry import *
-
 # Import Element wrapper extension methods
+import clr
 clr.AddReference("RevitNodes")
 import Revit
 clr.ImportExtensions(Revit.Elements)
-
-# Import geometry conversion extension methods
-clr.ImportExtensions(Revit.GeometryConversion)
 
 # Import DocumentManager and TransactionManager
 clr.AddReference("RevitServices")
 import RevitServices
 from RevitServices.Persistence import DocumentManager
-from RevitServices.Transactions import TransactionManager
-
 doc = DocumentManager.Instance.CurrentDBDocument
-uiapp = DocumentManager.Instance.CurrentUIApplication
-app = uiapp.Application
 
 # Import RevitAPI
 clr.AddReference("RevitAPI")
@@ -31,6 +21,10 @@ from Autodesk.Revit.DB import *
 import System
 from System import Array
 from System.Collections.Generic import *
+
+import sys
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
