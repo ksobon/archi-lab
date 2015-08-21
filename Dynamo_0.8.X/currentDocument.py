42984a083838be2ccb70b6c910d8699f7d408562
Konrad K Sobon <ksobon1986@gmail.com>
2015-08-21 10:53:11 -0400

    Update and rename Dynamo_0.7.X/currentDocument.py to Dynamo_0.8.X/currentDocument.py

---
+++ Dynamo_0.8.X/currentDocument.py
@@ -0,0 +1,14 @@
+# Copyright(c) 2015, Konrad K Sobon
+# @arch_laboratory, http://archi-lab.net
+
+# Import DocumentManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+doc = DocumentManager.Instance.CurrentDBDocument
+
+#The inputs to this node will be stored as a list in the IN variable.
+dataEnteringNode = IN
+
+#Assign your output to the OUT variable
+OUT = doc
