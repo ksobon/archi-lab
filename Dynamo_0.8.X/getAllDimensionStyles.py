538e42673ed6ac5042844c5958e6ba3d81c88db3
Konrad K Sobon <ksobon1986@gmail.com>
2015-08-21 13:19:07 -0400

    Update and rename getAllDimensionTypes.py to getAllDimensionStyles.py

--- Dynamo_0.8.X/getAllDimensionStyles.py
+++ Dynamo_0.8.X/getAllDimensionStyles.py
@@ -1,11 +1,8 @@
 #Copyright(c) 2015, Konrad K Sobon
 # @arch_laboratory, http://archi-lab.net
 
-import clr
-clr.AddReference('ProtoGeometry')
-from Autodesk.DesignScript.Geometry import *
-
 # Import DocumentManager and TransactionManager
+import clr
 clr.AddReference("RevitServices")
 import RevitServices
 from RevitServices.Persistence import DocumentManager
@@ -22,20 +19,33 @@ clr.AddReference("RevitNodes")
 import Revit
 clr.ImportExtensions(Revit.Elements)
 
+import sys
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
 
-dTypes = FilteredElementCollector(doc).OfClass(DimensionType)
-sTypes = FilteredElementCollector(doc).OfClass(SpotDimensionType)
-
-sTypesName = []
-for i in sTypes:
-	sTypesName.append(i.ToDSType(True).Name)
-
-dimensionTypes = []
-for i in dTypes:
-	if i.ToDSType(True).Name not in sTypesName:
-		dimensionTypes.append(i.ToDSType(True))
-
+try:
+	errorReport = None
+	dTypes = FilteredElementCollector(doc).OfClass(DimensionType)
+	sTypes = FilteredElementCollector(doc).OfClass(SpotDimensionType)
+	
+	sTypesName = []
+	for i in sTypes:
+		sTypesName.append(i.ToDSType(True).Name)
+	
+	dimensionTypes = []
+	for i in dTypes:
+		if i.ToDSType(True).Name not in sTypesName:
+			dimensionTypes.append(i.ToDSType(True))
+except:
+	# if error accurs anywhere in the process catch it
+	import traceback
+	errorReport = traceback.format_exc()
+	
 #Assign your output to the OUT variable
-OUT = dimensionTypes
+if errorReport == None:
+	OUT = dimensionTypes
+else:
+	OUT = errorReport
