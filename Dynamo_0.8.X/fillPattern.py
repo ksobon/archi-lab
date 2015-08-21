6c804343a2e3acd00c0397d3e76f3f9be718ab92
Konrad K Sobon <ksobon1986@gmail.com>
2015-08-21 15:56:57 -0400

    Update fillPattern.py

--- Dynamo_0.8.X/fillPattern.py
+++ Dynamo_0.8.X/fillPattern.py
@@ -11,7 +11,6 @@ clr.ImportExtensions(Revit.Elements)
 clr.AddReference("RevitServices")
 import RevitServices
 from RevitServices.Persistence import DocumentManager
-doc = DocumentManager.Instance.CurrentDBDocument
 
 # Import RevitAPI
 clr.AddReference("RevitAPI")
@@ -25,13 +24,23 @@ sys.path.append(pyt_path)
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
 
-name = str(IN[0])
+names = IN[0]
 target = IN[1]
+
+def GetFillPattern(name, target):
+	doc = DocumentManager.Instance.CurrentDBDocument
+	fillPat = FillPatternElement.GetFillPatternElementByName(doc, target, name)
+	return fillPat.ToDSType(True)
+
+def ProcessListArg(_func, _list, _arg):
+	return map( lambda x: ProcessListArg(_func, x, _arg) if type(x)==list else _func(x, _arg), _list )
 
 try:
 	errorReport = None
-	fillPat = FillPatternElement.GetFillPatternElementByName(doc, target, name)
-	fillPat = fillPat.ToDSType(True)
+	if isinstance(names, list):
+		fillPatterns = ProcessListArg(GetFillPattern, names, target)
+	else:
+		fillPatterns = GetFillPattern(names, target)
 except:
 	# if error accurs anywhere in the process catch it
 	import traceback
@@ -39,6 +48,6 @@ except:
 
 #Assign your output to the OUT variable
 if errorReport == None:
-	OUT = fillPat
+	OUT = fillPatterns
 else:
 	OUT = errorReport
