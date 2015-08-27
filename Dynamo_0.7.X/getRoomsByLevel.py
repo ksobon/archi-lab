5b35b50eababa0b416ca9ba5b988833ce4e7ccb6
Konrad K Sobon <ksobon1986@gmail.com>
2015-08-27 16:37:12 -0400

    Update getRoomsByLevel.py

--- Dynamo_0.7.X/getRoomsByLevel.py
+++ Dynamo_0.7.X/getRoomsByLevel.py
@@ -1,4 +1,4 @@
-#Copyright(c) 2015, Konrad K Sobon
+# Copyright(c) 2014, Konrad K Sobon
 # Grimshaw Architects, http://grimshaw-architects.com/
 # Archi-lab, http://wwww.archi-lab.net
 
@@ -17,6 +17,8 @@ clr.AddReference("RevitServices")
 import RevitServices
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
+doc =  DocumentManager.Instance.CurrentDBDocument
+app =  DocumentManager.Instance.CurrentUIApplication.Application
 
 # Import ToDSType(bool) extension method
 clr.AddReference("RevitNodes")
@@ -24,51 +26,66 @@ import Revit
 clr.ImportExtensions(Revit.Elements)
 clr.ImportExtensions(Revit.GeometryConversion)
 
+import sys
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
 #Import Collections
 from System.Collections.Generic import *
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
 
-doc =  DocumentManager.Instance.CurrentDBDocument
-app =  DocumentManager.Instance.CurrentUIApplication.Application
 levelInput = IN[0]
 filterToggle = IN[1]
-roomNames, validRooms = [], []
 
-collector = FilteredElementCollector(doc)
-levelFilter = ElementLevelFilter(UnwrapElement(levelInput).Id)
+try:
+	errorReport = None
+	roomNames, validRooms = [], []
+	
+	collector = FilteredElementCollector(doc)
+	levelFilter = ElementLevelFilter(UnwrapElement(levelInput).Id)
+	
+	areaFilter = AreaFilter()
+	areaExcludes = collector.WherePasses(areaFilter).ToElements()
+	areaExcludes = list(areaExcludes)
+	
+	element_set = ElementSet()
+	excludes = List[ElementId]()
+	if len(areaExcludes) == 0:
+		logicalFilter = levelFilter
+	else:
+		for i in areaExcludes:
+			element_set.Insert(i)
+			elemIter = element_set.ForwardIterator()
+			elemIter.Reset()
+			while elemIter.MoveNext():
+				curElem = elemIter.Current
+				excludes.Add(curElem.Id)
+		filter = ExclusionFilter(excludes)
+		logicalFilter = LogicalAndFilter(levelFilter, filter)
+	
+	bip = BuiltInParameter.ROOM_NAME
+	if filterToggle == False:
+		allRoomsOnLevel = FilteredElementCollector(doc).OfClass(Autodesk.Revit.DB.SpatialElement).WherePasses(logicalFilter).ToElements()
+		for i in allRoomsOnLevel:
+			roomNames.append(i.get_Parameter(bip).AsString())
+		output = allRoomsOnLevel, roomNames
+	else:
+		allRoomsOnLevel = FilteredElementCollector(doc).OfClass(Autodesk.Revit.DB.SpatialElement).WherePasses(logicalFilter).ToElements()
+		for i in allRoomsOnLevel:
+			if i.Area != 0:
+				validRooms.append(i)
+		for i in validRooms:
+			roomNames.append(i.get_Parameter(bip).AsString())
+		output = validRooms, roomNames
+except:
+	# if error accurs anywhere in the process catch it
+	import traceback
+	errorReport = traceback.format_exc()
 
-areaFilter = AreaFilter()
-areaExcludes = collector.WherePasses(areaFilter).ToElements()
-areaExcludes = list(areaExcludes)
-
-element_set = ElementSet()
-excludes = List[ElementId]()
-if len(areaExcludes) == 0:
-	logicalFilter = levelFilter
+#Assign your output to the OUT variable
+if errorReport == None:
+	OUT = output
 else:
-	for i in areaExcludes:
-		element_set.Insert(i)
-		elemIter = element_set.ForwardIterator()
-		elemIter.Reset()
-		while elemIter.MoveNext():
-			curElem = elemIter.Current
-			excludes.Add(curElem.Id)
-	filter = ExclusionFilter(excludes)
-	logicalFilter = LogicalAndFilter(levelFilter, filter)
-
-
-if filterToggle == False:
-	allRoomsOnLevel = FilteredElementCollector(doc).OfClass(Autodesk.Revit.DB.SpatialElement).WherePasses(logicalFilter).ToElements()
-	for i in allRoomsOnLevel:
-		roomNames.append(i.get_Parameter("Name").AsString())
-	OUT = allRoomsOnLevel, roomNames
-else:
-	allRoomsOnLevel = FilteredElementCollector(doc).OfClass(Autodesk.Revit.DB.SpatialElement).WherePasses(logicalFilter).ToElements()
-	for i in allRoomsOnLevel:
-		if i.Area != 0:
-			validRooms.append(i)
-	for i in validRooms:
-		roomNames.append(i.get_Parameter("Name").AsString())
-	OUT = validRooms, roomNames
+	OUT = errorReport
