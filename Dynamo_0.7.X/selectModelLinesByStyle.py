0534e6be1e09ec75deb914bdab512e393f2548bd
Konrad K Sobon <ksobon1986@gmail.com>
2015-09-01 12:02:56 -0400

    Update selectModelLinesByStyle.py

--- Dynamo_0.7.X/selectModelLinesByStyle.py
+++ Dynamo_0.7.X/selectModelLinesByStyle.py
@@ -1,4 +1,4 @@
-#Copyright(c) 2014, Konrad Sobon
+# Copyright(c) 2015, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net
 
 import clr
@@ -9,7 +9,6 @@ from Autodesk.DesignScript.Geometry import *
 clr.AddReference("RevitServices")
 import RevitServices
 from RevitServices.Persistence import DocumentManager
-from RevitServices.Transactions import TransactionManager
 
 # Import RevitAPI
 clr.AddReference("RevitAPI")
@@ -18,21 +17,30 @@ from Autodesk.Revit.DB import *
 from Autodesk.Revit.DB.BuiltInCategory import *
 
 doc = DocumentManager.Instance.CurrentDBDocument
-uiapp = DocumentManager.Instance.CurrentUIApplication
-app = uiapp.Application
+
+import sys
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
 
 styleName = IN[0]
 
-modelLines = []
-collector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Lines)
-paramIndex = Autodesk.Revit.DB.BuiltInParameter.BUILDING_CURVE_GSTYLE
-
-for i in collector:
-	if i.Name == "Model Lines" and i.LineStyle.Name == styleName:
-		modelLines.append(i)
-
+try:
+	errorReport = None
+	collector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Lines)
+	modelLines = []
+	for i in collector:
+		if i.Name == "Model Lines" and i.LineStyle.Name == styleName:
+			modelLines.append(i)
+except:
+	# if error accurs anywhere in the process catch it
+	import traceback
+	errorReport = traceback.format_exc()
+	
 #Assign your output to the OUT variable
-OUT = modelLines
+if errorReport == None:
+	OUT = modelLines
+else:
+	OUT = errorReport
