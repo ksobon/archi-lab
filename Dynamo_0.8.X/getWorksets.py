7547a9431ae1c64f21160811943ea9f65429b143
Konrad K Sobon <ksobon1986@gmail.com>
2015-09-01 12:18:41 -0400

    Update and rename Dynamo_0.7.X/getWorksets.py to Dynamo_0.8.X/getWorksets.py

--- Dynamo_0.8.X/getWorksets.py
+++ Dynamo_0.8.X/getWorksets.py
@@ -1,4 +1,4 @@
-#Copyright(c) 2015, Konrad Sobon
+# Copyright(c) 2015, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net
 
 import clr
@@ -20,19 +20,28 @@ import Revit
 from Autodesk.Revit.DB import *
 clr.ImportExtensions(Revit.Elements)
 
+import sys
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
 #The inputs to this node will be stored as a list in the IN variable.
-#doc = DocumentManager.Instance.CurrentDBDocument
-uiapp = DocumentManager.Instance.CurrentUIApplication
-app = uiapp.Application
+
 doc = IN[0]
 
-#create workset collector
-userWorksets = FilteredWorksetCollector(doc).OfKind(WorksetKind.UserWorkset)
-#extract workset's name and ids
-names, ids = [], []
-for i in userWorksets:
-	names.append(i.Name)
-	ids.append(i.Id)
-	
+try:
+	errorReport = None
+	userWorksets = FilteredWorksetCollector(doc).OfKind(WorksetKind.UserWorkset)
+	names, ids = [], []
+	for i in userWorksets:
+		names.append(i.Name)
+		ids.append(i.Id)
+except:
+	# if error accurs anywhere in the process catch it
+	import traceback
+	errorReport = traceback.format_exc()
+
 #Assign your output to the OUT variable
-OUT = userWorksets, names, ids
+if errorReport == None:
+	OUT = userWorksets, names, ids
+else:
+	OUT = errorReport
