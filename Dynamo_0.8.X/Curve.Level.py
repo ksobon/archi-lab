9d5700b069e5fa86946b9af03f5a98c3b4cf9460
Konrad K Sobon <ksobon1986@gmail.com>
2015-10-03 17:59:50 -0400

    Create Curve.Level.py

---
+++ Dynamo_0.8.X/Curve.Level.py
@@ -0,0 +1,45 @@
+# Copyright(c) 2015, Konrad K Sobon
+# @arch_laboratory, http://archi-lab.net
+
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+# Import DocumentManager and TransactionManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+doc = DocumentManager.Instance.CurrentDBDocument
+
+import sys
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+#The inputs to this node will be stored as a list in the IN variable.
+dataEnteringNode = IN
+
+def ProcessList(_func, _list):
+    return map( lambda x: ProcessList(_func, x) if type(x)==list else _func(x), _list )
+
+def GetLevel(e):
+	doc = DocumentManager.Instance.CurrentDBDocument
+	return doc.GetElement(UnwrapElement(e).LevelId)
+
+if isinstance(IN[0], list):
+	lines = IN[0]
+else:
+	lines = [IN[0]]
+
+try:
+	errorReport = None
+	output = ProcessList(GetLevel, lines)
+except:
+	# if error accurs anywhere in the process catch it
+	import traceback
+	errorReport = traceback.format_exc()
+
+#Assign your output to the OUT variable
+if errorReport == None:
+	OUT = output
+else:
+	OUT = errorReport
