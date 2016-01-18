a5fa3c2944136161e50f385e08ae79c514fd60fa
Konrad K Sobon <ksobon1986@gmail.com>
2016-01-17 20:24:15 -0500

    Update getFloorArea.py

--- getFloorArea.py
+++ getFloorArea.py
@@ -1,11 +1,8 @@
-#Copyright(c) 2015, Konrad K Sobon
+# Copyright(c) 2016, Konrad K Sobon
 # @arch_laboratory, http://archi-lab.net
 
-import clr
-clr.AddReference('ProtoGeometry')
-from Autodesk.DesignScript.Geometry import *
-
 # Import RevitAPI
+import clr
 clr.AddReference("RevitAPI")
 import Autodesk
 from Autodesk.Revit.DB import *
@@ -13,17 +10,19 @@ from Autodesk.Revit.DB import *
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
 
-elements = []
-for i in IN[0]:
-	elements.append(UnwrapElement(i))
+def Unwrap(item):
+	return UnwrapElement(item)
 
-def process_list(_func, _list):
+def ProcessList(_func, _list):
     return map( lambda x: process_list(_func, x) if type(x)==list else _func(x), _list )
 
 def FloorArea(item):
-	paramName = BuiltInParameter.HOST_AREA_COMPUTED
-	param = item.get_Parameter(paramName).AsValueString()
-	return param
+	return item.get_Parameter(BuiltInParameter.HOST_AREA_COMPUTED).AsValueString()
+
+if isinstance(IN[0], list):
+	elements = ProcessList(Unwrap, IN[0])
+else:
+	elements = [Unwrap(IN[0])]
 
 #Assign your output to the OUT variable
-OUT = process_list(FloorArea, elements)
+OUT = ProcessList(FloorArea, elements)
