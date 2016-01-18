9b9c217868cbe11517ab410c1c060aa617ac81bc
Konrad K Sobon <ksobon1986@gmail.com>
2016-01-17 21:04:05 -0500

    Update getSurveyPoint.py

--- getSurveyPoint.py
+++ getSurveyPoint.py
@@ -1,4 +1,4 @@
-#Copyright(c) 2015, Konrad Sobon
+# Copyright(c) 2016, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net
 
 import clr
@@ -9,23 +9,16 @@ from Autodesk.DesignScript.Geometry import *
 clr.AddReference("RevitServices")
 import RevitServices
 from RevitServices.Persistence import DocumentManager
+doc = DocumentManager.Instance.CurrentDBDocument
 
 # Import RevitAPI
 clr.AddReference("RevitAPI")
 import Autodesk
 from Autodesk.Revit.DB import *
 
-doc = DocumentManager.Instance.CurrentDBDocument
-
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
 inPoints = IN[0]
-
-def toDynPoint(point):
-	x = Autodesk.Revit.DB.UnitUtils.Convert(point.X, DisplayUnitType.DUT_DECIMAL_FEET, DisplayUnitType.DUT_METERS)
-	y = Autodesk.Revit.DB.UnitUtils.Convert(point.Y, DisplayUnitType.DUT_DECIMAL_FEET, DisplayUnitType.DUT_METERS)
-	z = Autodesk.Revit.DB.UnitUtils.Convert(point.Z, DisplayUnitType.DUT_DECIMAL_FEET, DisplayUnitType.DUT_METERS)
-	return Autodesk.DesignScript.Geometry.Point.ByCoordinates(x,y,z)
 
 surveyPoints = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_SharedBasePoint).ToElements()
 
@@ -37,4 +30,4 @@ eastWest = surveyPoints[0].get_Parameter(bipEW).AsDouble()
 northSouth = surveyPoints[0].get_Parameter(bipNS).AsDouble()
 elev = surveyPoints[0].get_Parameter(bipElev).AsDouble()
 
-OUT = toDynPoint(XYZ(eastWest, northSouth, elev))
+OUT = Autodesk.DesignScript.Geometry.Point.ByCoordinates(eastWest, northSouth, elev)
