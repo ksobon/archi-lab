1ce457e7bd2a80b43ec633297c0b656296943e89
Konrad K Sobon <ksobon1986@gmail.com>
2016-04-05 18:39:51 -0400

    Update and rename Dynamo_0.7.X/dispatch.py to Dynamo_0.9.X/dispatch.py

--- Dynamo_0.9.X/dispatch.py
+++ Dynamo_0.9.X/dispatch.py
@@ -1,9 +1,6 @@
-#Copyright(c) 2015, Konrad K Sobon
+# Copyright(c) 2016, Konrad K Sobon
 # @arch_laboratory, http://archi-lab.net
 
-import clr
-clr.AddReference('ProtoGeometry')
-from Autodesk.DesignScript.Geometry import *
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
 
