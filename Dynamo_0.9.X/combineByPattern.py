fa323d0bc1105a94e854b0cef29a5dcde5ebc0e6
Konrad K Sobon <ksobon1986@gmail.com>
2016-04-05 18:45:37 -0400

    Update and rename Dynamo_0.7.X/combineByPattern.py to Dynamo_0.9.X/combineByPattern.py

--- Dynamo_0.9.X/combineByPattern.py
+++ Dynamo_0.9.X/combineByPattern.py
@@ -1,17 +1,14 @@
-#Copyright(c) 2015, Konrad K Sobon
+# Copyright(c) 2016, Konrad K Sobon
 # @arch_laboratory, http://archi-lab.net
 
-import clr
-clr.AddReference('ProtoGeometry')
-from Autodesk.DesignScript.Geometry import *
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
 
 lst_1 = IN[0]
 lst_2 = IN[1]
 pattern = IN[2]
-result = []
 
+result = []
 for i, j, k in zip(lst_1, lst_2, pattern):
 	if k == False:
 		result.append(i)
