f2fe60b7d78af17425d200bf05cd9f6fbb7987ee
Konrad K Sobon <ksobon1986@gmail.com>
2016-12-12 12:40:26 -0500

    Update getWorksets.py

--- Dynamo_0.8.X/getWorksets.py
+++ Dynamo_0.8.X/getWorksets.py
@@ -1,11 +1,8 @@
-# Copyright(c) 2015, Konrad Sobon
+# Copyright(c) 2016, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net
 
-import clr
-clr.AddReference('ProtoGeometry')
-from Autodesk.DesignScript.Geometry import *
-
 # Import DocumentManager
+import clr
 clr.AddReference("RevitServices")
 import RevitServices
 from RevitServices.Persistence import DocumentManager
@@ -20,13 +17,13 @@ import Revit
 from Autodesk.Revit.DB import *
 clr.ImportExtensions(Revit.Elements)
 
+doc = DocumentManager.Instance.CurrentDBDocument
+
 import sys
 pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
 
 #The inputs to this node will be stored as a list in the IN variable.
-
-doc = IN[0]
 
 try:
 	errorReport = None
